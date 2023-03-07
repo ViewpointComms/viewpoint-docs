e4c06b03344f2de1be7dd75627c7096850dc8746
Tom Hirst <109214272+Tom-Hirst@users.noreply.github.com>
2023-03-07 18:05:04 +0000

    Added astro aside and updated content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,29 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
+import AutoImport from 'astro-auto-import';
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
+import { asideAutoImport, astroAsides } from './integrations/astro-asides';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    AutoImport({
+			imports: [asideAutoImport],
+		}),
   // Enable Preact to support Preact JSX components.
   preact(),
   // Enable React for the Algolia search component.
-  react(), mdx()],
-  site: `https://docs.viewpointcomms.com`
+  react(),
+  astroAsides(),
+  mdx()],
+  site: `https://docs.viewpointcomms.com`,
+  build: {
+    rollupOptions: {
+      external: ['./src/components/Aside.astro']
+    }
+  }
 });
+
